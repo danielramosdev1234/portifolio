f5e0c0121eafcc84bb4b3c7ffa5d3126f1b8f5ab
danielramosdev1234 <danielramosdev36@gmail.com>
2025-09-12 13:03:28 -0300

    Criar arquivo Home.js na página src/pages

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <section>
+      <h1>Bem-vindo ao Projeto React!</h1>
+      <p>Esta é sua tela principal. Use o menu para navegar pelas funcionalidades futuras.</p>
+    </section>
+  );
+}
+
+export default Home;
